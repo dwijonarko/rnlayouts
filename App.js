f4f1b97d641a2935d5e658b1b567186d7edd2819
Dwi Wijonarko <d.wijonarko@gmail.com>
2020-04-16 18:56:55 +0700

    profile layout

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const App: () => React$Node = () => {
   return (
     <>
-    <LoginScreen />
+    <HomeScreen />
     </>
   );
 };
---
+++ screens/HomeScreen.js
@@ -0,0 +1,148 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import {Header, Avatar, ListItem} from 'react-native-elements';
+const HomeScreen = () => {
+  const list = [
+    {
+      title: 'Mobile Application',
+      icon: 'android',
+    },
+    {
+      title: 'Web Application',
+      icon: 'web',
+    },
+    {
+      title: 'Landing Page',
+      icon: 'flight-takeoff',
+    },
+    {
+      title: 'Poster',
+      icon: 'palette',
+    },
+    {
+      title: 'Logo',
+      icon: 'pages',
+    },
+  ];
+  return (
+    <View style={{flex: 1}}>
+      <LinearGradient
+        colors={['#FE6869', '#FD6867']}
+        style={styles.linearGradient}>
+        <Header
+          containerStyle={styles.header}
+          leftComponent={{icon: 'menu', color: '#fff'}}
+          centerComponent={{text: '@dwijonarko', style: {color: '#fff'}}}
+          rightComponent={{icon: 'home', color: '#fff'}}
+        />
+        <View style={styles.avatarBox}>
+          <View style={styles.avatarBorder}>
+            <Avatar
+              size="large"
+              rounded
+              source={{
+                uri:
+                  'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+              }}
+            />
+          </View>
+        </View>
+      </LinearGradient>
+      <View style={styles.infoBox}>
+        <View style={styles.info}>
+          <View style={[styles.infoText, styles.infoTextLeft]}>
+            <Text style={styles.infoTitle}>183</Text>
+            <Text style={styles.infoSubTitle}>shoots</Text>
+          </View>
+          <View style={styles.infoText}>
+            <Text style={styles.infoTitle}>9778</Text>
+            <Text style={styles.infoSubTitle}>followers</Text>
+          </View>
+          <View style={[styles.infoText, styles.infoTextRight]}>
+            <Text style={styles.infoTitle}>100</Text>
+            <Text style={styles.infoSubTitle}>projects</Text>
+          </View>
+        </View>
+        <View
+          style={styles.listItem}>
+          {list.map((item, i) => (
+            <ListItem
+              titleStyle={styles.listItemTitle}
+              key={i}
+              title={item.title}
+              leftIcon={{name: item.icon}}
+              bottomDivider
+              chevron={{color: '#FE6869'}}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+var styles = StyleSheet.create({
+  linearGradient: {
+    flex: 1,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignContent: 'flex-end',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  header: {
+    backgroundColor: '#FE6869',
+    justifyContent: 'space-around',
+    borderBottomColor: '#FD6867',
+  },
+  avatarBox: {flex: 1, flexDirection: 'column', justifyContent: 'center'},
+  avatarBorder: {
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#fefefe',
+    borderRadius: 100,
+  },
+  info: {
+    minHeight: 40,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'space-between',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 3.27,
+    elevation: 2,
+  },
+  infoText: {
+    alignItems: 'center',
+    paddingHorizontal: 15,
+  },
+  infoTextLeft: {
+    borderRightWidth: 1,
+    borderRightColor: '#cecece',
+  },
+  infoTextRight: {
+    borderLeftWidth: 1,
+    borderLeftColor: '#cecece',
+  },
+  infoBox: {flex: 2, marginVertical: 20, alignItems: 'center'},
+  infoTitle: {fontSize: 26, fontWeight: 'bold', color: 'grey'},
+  infoSubTitle:{fontSize: 10, color: 'grey'},
+  listItem:{marginVertical: 20, paddingHorizontal: 10, width: '100%'},
+  listItemTitle:{color: '#ff8a65', fontWeight: 'bold'}
+});
